c06e8954ebf7fe94bc4b66a3c940a0d4dc9f2de0
Bruno Herfst <bruno@eness.com>
2021-09-14 16:15:00 +1000

    First drawer

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,23 +1,103 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void ofApp::setup() {
+	ofEnableAntiAliasing();
+
+	oneShot   = false;
+	drawImage = false;
+
+	showImage = false;
+
+	penColor = ofColor(0, 0, 0, 200);
+	paperColor = ofColor(250, 250, 250);
+
+	img_name = "travel-bubbles-covid-19";
+	img_ext  = "jpg";
+	img.load("src_img/"+ img_name + "." + img_ext);
+
+	(img.getWidth() > img.getHeight()) ? isLandscape = true : isLandscape = false;
+
+	imgRatio = float(img.getWidth()) / float(img.getHeight());
+	img.resize(ofGetHeight()* imgRatio, ofGetHeight());
+	
+	tilesX = 120;
+	tilesY = imgRatio * tilesX;
+	ofSetBackgroundAuto(false);
+	ofBackground(paperColor);
+	tileSize = float(ofGetWidth()) / tilesX;
+	tileAddon = 10;
+	
+	penWidth = 1.5;
 
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+	
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
+	
+	if (drawImage) {
+		
+		ofBackground(paperColor);
+		
+		if (showImage) {
+			img.draw(0, 0);
+		}
+
+		if (oneShot) {
+			ofBeginSaveScreenAsPDF("pixelplotted_" + img_name + "_" + ofGetTimestampString() + ".pdf", false);
+		}
+		
+		ofNoFill();
+		ofSetLineWidth(penWidth);
+		ofSetColor(penColor);
+
+		pixels = img.getPixels();
+		int w = img.getWidth();
+		int h = img.getHeight();
+		int yCount = 0;
+		for (int y = 0; y < h; y += tileSize) {
+			yCount++;
+			int xCount = 0;
+			for (int x = 0; x < w; x += tileSize) {
+				xCount++;
+				ofColor c = pixels.getColor(x, y);
+				float l = c.getLightness();
+
+				float size = (((180.0f - l) / 255.0f)*tileSize)+tileAddon;
+
+
+				float cx = (xCount * tileSize) - (size / 2);
+				float cy = (yCount * tileSize) - (size / 2);
+				ofPushMatrix();
+				ofTranslate(cx, cy, 0);
+				ofRotateZDeg(l);
+				ofDrawRectangle(0, 0, size, size);
+				ofPopMatrix();
+				
+				
+			}
+		}
+
+		drawImage = false;
+		oneShot = false;
+	}
 
 }
 
 //--------------------------------------------------------------
-void ofApp::keyPressed(int key){
+void ofApp::drawPixel(float size, ofColor color) {
+	int x = 0;
+}
 
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+	drawImage = true;
+	oneShot = true;
 }
 
 //--------------------------------------------------------------
@@ -26,7 +106,7 @@ void ofApp::keyReleased(int key){
 }
 
 //--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y){
+void ofApp::mouseMoved(int x, int y ){
 
 }
 
--- src/ofApp.h
+++ src/ofApp.h
@@ -3,14 +3,16 @@
 #include "ofMain.h"
 
 class ofApp : public ofBaseApp{
+
 	public:
 		void setup();
 		void update();
 		void draw();
-		
+		void drawPixel(float size, ofColor color);
+
 		void keyPressed(int key);
 		void keyReleased(int key);
-		void mouseMoved(int x, int y);
+		void mouseMoved(int x, int y );
 		void mouseDragged(int x, int y, int button);
 		void mousePressed(int x, int y, int button);
 		void mouseReleased(int x, int y, int button);
@@ -19,4 +21,19 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+
+		ofImage img;
+		ofPixels pixels;
+		ofMesh mesh;
+		ofColor penColor, paperColor;
+
+		string img_name, img_ext;
+
+		float imgRatio;
+		float tilesX, tilesY, tileSize, tileAddon;
+		float penWidth;
+
+		bool drawImage, showImage, isLandscape;
+		bool oneShot;
+	
 };
