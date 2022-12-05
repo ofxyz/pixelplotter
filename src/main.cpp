96894335a9ceb3834be80aec2c0db30a555f3617
Bruno Herfst <bruno.herfst@gmail.com>
2022-12-05 18:27:58 +1100

    more sensible size

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,6 @@
 
 //========================================================================
 int main( ){
-	ofSetupOpenGL(2160, 2160, OF_WINDOW);
+	ofSetupOpenGL(2048, 2048, OF_WINDOW);
 	ofRunApp(new ofApp());
 }
