940e085041dd12ec495c84273ca0ab20eb40d6e8
Bruno Herfst <bruno.herfst@gmail.com>
2024-04-24 13:59:39 +1000

    Back to working thank to @Vairn

--- src/main.cpp
+++ src/main.cpp
@@ -10,7 +10,7 @@ int main() {
 	settings.setGLESVersion(2);
 #else
 	// Programmable pipeline
-	//settings.setGLVersion(3, 2); 
+	settings.setGLVersion(3, 2); 
 #endif
 
 	settings.title = ofApp::getWindowTitle();
